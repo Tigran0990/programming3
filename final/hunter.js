647552e4a6463b2bbde59c45436b6af5169e8cc7
Tigran0990 <tigran.poghosyan@tumo.org>
2022-10-04 16:43:33 +0400

    first commit

---
+++ final/hunter.js
@@ -0,0 +1,129 @@
+class Hunter {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.directions = [];
+
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    mul() {
+        let emptyCelss = this.chooseCell(0)
+        let newCell = random(emptyCelss)
+        if (newCell) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = 3
+            let newGr = new Hunter(newX, newY)
+            HunterArr.push(newGr)
+            this.energy = 35
+        }
+    }
+
+    move() {
+        this.energy--
+        let emptyCelss = this.chooseCell(0)
+        let newCell = random(emptyCelss)
+        if (newCell && this.energy >= 0) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else {
+            this.die()
+        }
+    }
+
+    eat() {
+        let emptyCelss = this.chooseCell(2)
+        let emtptyCelss1 = this.chooseCell(3)
+        let newCell = random(emptyCelss)
+        let newCell1 = random(emtptyCelss1)
+        if (newCell) {
+            this.energy++
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            if (this.energy >= 30) {
+                this.mul()
+            }
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }       
+            }
+
+        } else if (newCell1) {
+            this.energy++
+            let newX = newCell1[0]
+            let newY = newCell1[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            if (this.energy >= 30) {
+                this.mul()
+            }
+            for (var i in PredatorArr) {
+                if (newX == PredatorArr[i].x && newY == PredatorArr[i].y) {
+                    PredatorArr.splice(i, 1);
+                    break;
+                }
+            }    
+
+        } else {
+            this.move()
+
+        }
+
+        }
+    
+
+
+
+
+    die() {
+        matrix[this.y][this.x] = 0
+        for (var i in HunterArr) {
+            if (this.x == HunterArr[i].x && this.y == HunterArr[i].y) {
+                HunterArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
+
+
---
+++ final/infected.js
@@ -0,0 +1,51 @@
+class Infected {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 10;
+        this.directions = [];
+
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+    
+
+    move() {
+        let emptyCelss = this.chooseCell(0)
+        let newCell = random(emptyCelss)
+        if (newCell && this.energy >= 0) {
+            let newX = newCell[0]
+            let newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+    }
+}
